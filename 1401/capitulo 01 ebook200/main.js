ef231f76501c470283570fda16c68dd9b73d99e1
Paulo <paulocorreapc93@gmail.com>
2024-01-14 17:07:28 -0400

    exercícios 2 e 3 do livro 200 questoes em JS

--- 1401/capitulo 01 ebook200/main.js
+++ 1401/capitulo 01 ebook200/main.js
@@ -4,3 +4,15 @@ let idade = 30
 let peso = 103.2
 
 console.log(`idade: ${idade} / peso: ${peso}`)
+
+// exercício 02
+
+const pi = 3.14
+
+console.log(`o valor de pi é: ${pi}`)
+
+// exercício 03
+
+let valorMaximo = Number.MAX_VALUE
+
+console.log(`o valor máximo do JS é: ${valorMaximo}`)
