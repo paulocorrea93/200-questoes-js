2e8b578f3ba73ab3ca51d9029af6f25ede6e0a61
Paulo <paulocorreapc93@gmail.com>
2024-01-14 17:16:58 -0400

    exercícios 4 e 7 do livro 200 questoes em JS

--- 1401/capitulo 01 ebook200/main.js
+++ 1401/capitulo 01 ebook200/main.js
@@ -1,18 +1,54 @@
-// exercício 01
+// exercício 01 - variáveis
 
 let idade = 30
 let peso = 103.2
 
 console.log(`idade: ${idade} / peso: ${peso}`)
 
-// exercício 02
+// exercício 02 - constantes
 
 const pi = 3.14
 
 console.log(`o valor de pi é: ${pi}`)
 
-// exercício 03
+// exercício 03 - maior valor JS suportado
 
 let valorMaximo = Number.MAX_VALUE
 
 console.log(`o valor máximo do JS é: ${valorMaximo}`)
+
+// exercício 04 - concatenação de variáveis
+
+let nome = "Paulo"
+let sobrenome = "Correa"
+
+const nomeCompleto = nome + " " + sobrenome;
+
+console.log(nomeCompleto)
+
+// exercício 05 - operador de adição
+
+let a = 10
+let b = 20
+
+console.log(a+b)
+
+// exercício 06 - valores booleanos
+
+const afirmacao01 = true
+const afirmacao02 = false
+
+console.log(afirmacao01||afirmacao02)
+console.log(afirmacao01&&afirmacao02)
+console.log(!afirmacao01)
+
+// exercício 07 - operadores de comparação
+
+let a1 = 5
+let a2 = 6
+let a3 = 7
+
+console.log(a1 > a2)
+console.log(a1 < a2)
+console.log(a1 === a2)
+
