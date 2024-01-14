be24da85a3abefb42517f55d2b00478c518b8cf3
Paulo <paulocorreapc93@gmail.com>
2024-01-14 17:36:35 -0400

    exercício 10 - verificar através da media de notas se o aluno passou de ano ou não

--- 1401/operadores condicionais/main.js
+++ 1401/operadores condicionais/main.js
@@ -9,3 +9,33 @@ if(num1 > 0){
 } else {
     console.log(`${num1} é neutro`)
 }
+
+// outra alternativa
+
+function verificarNumero (x){
+    if(x > 0){
+        console.log(`${x} é positivo`)
+    } else if (x < 0){
+        console.log(`${x} é negativo`)
+    } else {
+        console.log(`${x} é neutro`)
+    }   
+}
+
+verificarNumero(1)
+verificarNumero(0)
+verificarNumero(-1)
+
+// exercício 10
+
+function mediaAluno (nome, x,y){
+    media = (x+y)/2
+    if(media >= 7){
+        console.log(`${nome}, sua média foi ${media}, você foi aprovado.`)
+    } else {
+        console.log(`reprovado, ${nome} sua média foi ${media}`)
+    }
+}
+
+mediaAluno("Paulo",10, 8)
+mediaAluno("João",4, 8)
