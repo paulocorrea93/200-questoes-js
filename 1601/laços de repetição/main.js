1f434274d32ae9709d0841df2054548a35c966e1
Paulo <paulocorreapc93@gmail.com>
2024-01-16 12:48:20 -0400

    exercício 24 e 25 - 1 até 10 com for e 10 até 1 com while

---
+++ 1601/laços de repetição/main.js
@@ -0,0 +1,13 @@
+// exercício 24 - numero de 1 a 10
+
+for(x = 1; x <= 10; x++){
+  console.log(x)
+}
+
+// exercício 25 - 10 até 1 com while
+
+n1 = 10
+while(n1 >= 1){
+  console.log(n1)
+  n1--
+}
