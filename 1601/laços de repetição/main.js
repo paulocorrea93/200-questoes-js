89c9c5c5dd91a975380e928e009b6d61c59e377f
Paulo <paulocorreapc93@gmail.com>
2024-01-16 12:59:44 -0400

    correção, eram apenas número ímpares

--- 1601/laços de repetição/main.js
+++ 1601/laços de repetição/main.js
@@ -1,22 +1,24 @@
-// exercício 24 - numero de 1 a 10
+// // exercício 24 - numero de 1 a 10
 
-for(x = 1; x <= 10; x++){
-  console.log(x)
-}
+// for(x = 1; x <= 10; x++){
+//   console.log(x)
+// }
 
-// exercício 25 - 10 até 1 com while
+// // exercício 25 - 10 até 1 com while
 
-n1 = 10
-while(n1 >= 1){
-  console.log(n1)
-  n1--
-}
+// n1 = 10
+// while(n1 >= 1){
+//   console.log(n1)
+//   n1--
+// }
 
-// exercício 26 - 1 até 20 com do while
+// exercício 26 - 1 até 20 com do while, apenas número ímpares
 
 n2 = 1
 
 do{
-  console.log(n2)
-  n2++
-}while(n2 <= 20)
+    if(n2 % 2 != 0){
+      console.log(n2)
+    }
+    n2++
+} while (n2 <= 20)
