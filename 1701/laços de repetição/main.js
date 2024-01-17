7d7c71dc709b21f7a4db99214ad9e5393e7bea29
Paulo <paulocorreapc93@gmail.com>
2024-01-17 11:44:55 -0400

    exercício 30 - números primos de 1 a 100 com laço for

--- 1701/laços de repetição/main.js
+++ 1701/laços de repetição/main.js
@@ -17,3 +17,20 @@ do {
   if (pares % 2 == 0) console.log(pares);
   pares++;
 } while (pares <= 100);
+
+// exercício números primos de 1 a 100 com laço for
+
+for(x = 2; x <= 100; x++){
+  let primo = true
+
+  for(y = 2; y < x; y++){
+    if(x % y === 0){
+      primo = false
+      break
+    }
+  }
+
+  if(primo){
+    console.log(x)
+  }
+}
