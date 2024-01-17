e46b8b452d6779e41ee60adc825148bbe9304844
Paulo <paulocorreapc93@gmail.com>
2024-01-17 11:22:36 -0400

    exercício 29 - numero pares de 1 a 100 com do while

--- 1701/laços de repetição/main.js
+++ 1701/laços de repetição/main.js
@@ -1,11 +1,19 @@
 // exercício 28 - usando while para somar todos os numeros de 1 a 100
 
-let x = 1
-let novoX = 0
+let x = 1;
+let novoX = 0;
 
-while(x <= 100){
-  novoX+=x
-  x++
+while (x <= 100) {
+  novoX += x;
+  x++;
 }
-console.log(novoX)
+console.log(novoX);
 
+// exercício 29 - numero pares de 1 a 100 com do while
+
+let pares = 1;
+
+do {
+  if (pares % 2 == 0) console.log(pares);
+  pares++;
+} while (pares <= 100);
