0f3de80ffb4a7ac1d5c72223552f3619a764e04d
Paulo <paulocorreapc93@gmail.com>
2024-01-17 13:18:34 -0400

    exercício 31 - adivinhar numero entre 1 e 100 com while

--- 1701/laços de repetição/main.js
+++ 1701/laços de repetição/main.js
@@ -18,19 +18,37 @@ do {
   pares++;
 } while (pares <= 100);
 
-// exercício números primos de 1 a 100 com laço for
+// exercício 30 - números primos de 1 a 100 com laço for
 
-for(x = 2; x <= 100; x++){
-  let primo = true
+for (x = 2; x <= 100; x++) {
+  let primo = true;
 
-  for(y = 2; y < x; y++){
-    if(x % y === 0){
-      primo = false
-      break
+  for (y = 2; y < x; y++) {
+    if (x % y === 0) {
+      primo = false;
+      break;
     }
   }
 
-  if(primo){
-    console.log(x)
+  if (primo) {
+    console.log(x);
   }
 }
+
+// exercício 31 - adivinhar numero entre 1 e 100 com while
+
+let min = 1;
+let max = 100;
+let chute = Math.floor((min + max) / 2);
+let numeroPensado = 12;
+
+while(chute!=numeroPensado){
+  if(chute > numeroPensado){
+    max = chute
+  } else {
+    min = chute
+  }
+  chute = Math.floor((min+max)/2)
+}
+
+console.log(`O número pensado é: ${chute}`)
