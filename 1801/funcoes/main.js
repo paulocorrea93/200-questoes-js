307b942808d8f0faaf4d24c06646788533e48895
Paulo <paulocorreapc93@gmail.com>
2024-01-18 13:56:36 -0400

    exercício 45 e 46, ola mundo e somar 2 numeros

---
+++ 1801/funcoes/main.js
@@ -0,0 +1,15 @@
+// exercício 45 - funcao "ola mundo"
+
+function olaMundo(palavra){
+  console.log(palavra)
+}
+
+olaMundo("Olá mundo")
+
+// exercicio 46 - somar dois numeros
+
+function somar2(n1, n2){
+console.log(n1+n2)
+}
+
+somar2(4, 3)
