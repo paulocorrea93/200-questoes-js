95d9318d3aa328a1f0c7db9557001367fa5fc956
Paulo <paulocorreapc93@gmail.com>
2024-01-19 09:17:09 -0400

    exercícios de funções

--- 1901/funcoes/main.js
+++ 1901/funcoes/main.js
@@ -19,3 +19,53 @@ function nomeJoao(){
 }
 
 cumprimentar(nomeJoao)
+
+// exercício 01 - função basica
+
+function saudacao(nome){
+  console.log(`Seja bem-vindo, ${nome}.`)
+}
+
+saudacao("Paulo")
+
+// exercício 02 - mais de 1 parametro
+
+function soma(n1, n2){
+  console.log(n1+n2)
+}
+
+soma(2, 5)
+
+// exercício 03 - par ou impar
+
+function parImpar(n){
+  if(n % 2 == 0){
+    console.log(`${n} é par.`)
+  } else {
+    console.log(`${n} é impar.`)
+  }
+}
+
+parImpar(4)
+parImpar(9)
+
+// exercício 04 - contagem regressiva
+
+function regressiva(n){
+  for(let x = n; x >= 0; x--){
+    console.log(x)
+  }
+}
+
+regressiva(5)
+regressiva(14)
+
+// exercício 05 - função e escopo de variavel
+
+function testeVariavelEscopo(){
+  let teste = "variavel local"
+}
+
+console.log(teste)
+
+// variável não acessada, pois está acessível apenas dentro da função
