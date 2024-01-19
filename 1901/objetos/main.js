a5808f791a535fa4179ef1f0df51513b9f36c370
Paulo <paulocorreapc93@gmail.com>
2024-01-19 09:41:42 -0400

    manipulação de objeto

--- 1901/objetos/main.js
+++ 1901/objetos/main.js
@@ -1,10 +1,25 @@
 // objetos
 
 let carro = {
-  marca: 'fiat',
+  marca: "fiat",
   ano: 2023,
-  modelo: 'palio',
+  modelo: "palio",
   ligar: () => {
-    console.log(`ligado`)
-  }
-}
+    console.log(`ligado`);
+  },
+};
+
+console.log(carro.marca);
+console.log(carro["modelo"]);
+
+carro.cor = "prata"
+
+console.log(carro.cor)
+
+carro.modelo = "uno"
+
+console.log(carro.modelo)
+
+delete carro.marca
+
+console.log(carro.marca)
