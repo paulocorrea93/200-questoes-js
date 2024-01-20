793af47f8dd0059e9650a30ed63fb4e1f82f6dfd
Paulo <paulocorreapc93@gmail.com>
2024-01-20 17:23:37 -0400

    exercício 03

--- 2001/exerciciosObjArr/main.js
+++ 2001/exerciciosObjArr/main.js
@@ -3,15 +3,27 @@
 let estudante = {
   nome: "Paulo",
   idade: 30,
-  curso: "Nutrição"
-}
+  curso: "Nutrição",
+};
 
-estudante.idade = 31
+estudante.idade = 31;
 
-console.log(estudante)
+console.log(estudante);
 
 // exercício 02 - adicionando propriedades ao objeto
 
-estudante.semestre = 4
+estudante.semestre = 4;
 
-console.log(estudante)
+console.log(estudante);
+
+// exercício 03 - manipulação de arrays
+
+let frutas = ["mamãe", "limão", "pessego", "morango", "abacaxi"];
+
+frutas.push("kiwi", "melão", "ameixa");
+
+console.log(frutas);
+
+frutas.shift()
+
+console.log(frutas);
