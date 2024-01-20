71482558ed524a3ae70bd22b8ec5eae7b1c47bde
Paulo <paulocorreapc93@gmail.com>
2024-01-20 17:17:47 -0400

    exercício 1

---
+++ 2001/exerciciosObjArr/main.js
@@ -0,0 +1,11 @@
+// exercício 1 - criar estudante, com idade, curso e alterar idade
+
+let estudante = {
+  nome: "Paulo",
+  idade: 30,
+  curso: "Nutrição"
+}
+
+estudante.idade = 31
+
+console.log(estudante)
