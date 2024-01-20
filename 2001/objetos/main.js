7110a1eba23a2c19043c175b4064cc123cebc10e
Paulo <paulocorreapc93@gmail.com>
2024-01-20 17:11:53 -0400

    metodos de iteração - map

--- 2001/objetos/main.js
+++ 2001/objetos/main.js
@@ -1,9 +1,18 @@
 // metodos de iteração
 
-//forEach
+//forEach()
 
 let frutas = ['melancia', 'uva', 'kiwi', 'morango', 'pessego']
 
 frutas.forEach((frutas) => {
   console.log(frutas)
+})
+
+//map()
+
+let numeros = [1, 2, 3, 4, 5]
+
+let dobro = numeros.map((n) => {
+  let dbl = n*2
+  console.log(dbl)
 })
