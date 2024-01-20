86e89f8acd683905714483fa68e6adece5fcf905
Paulo <paulocorreapc93@gmail.com>
2024-01-20 17:14:54 -0400

    metodos de iteração - filter

--- 2001/objetos/main.js
+++ 2001/objetos/main.js
@@ -2,17 +2,26 @@
 
 //forEach()
 
-let frutas = ['melancia', 'uva', 'kiwi', 'morango', 'pessego']
+let frutas = ["melancia", "uva", "kiwi", "morango", "pessego"];
 
 frutas.forEach((frutas) => {
-  console.log(frutas)
-})
+  console.log(frutas);
+});
 
 //map()
 
-let numeros = [1, 2, 3, 4, 5]
+let numeros = [1, 2, 3, 4, 5];
 
 let dobro = numeros.map((n) => {
-  let dbl = n*2
-  console.log(dbl)
-})
+  let dbl = n * 2;
+  console.log(dbl);
+});
+
+// filter()
+
+let numerosFilter = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+numerosFilter.filter((n) => {
+  n % 2 == 0;
+  return numerosFilter;
+});
