6fddb31163739bbc67d31f1f15796dba1207f57f
Paulo <paulocorreapc93@gmail.com>
2024-01-21 17:21:40 -0400

    adicionando classe ao elemento

--- 2101/DOM - Eventos/main.js
+++ 2101/DOM - Eventos/main.js
@@ -3,3 +3,9 @@ let titleExem = document.getElementById('main-header')
 titleExem.style.color = 'grey'
 titleExem.style.fontSize = '40px'
 titleExem.style.background = 'black'
+
+// adicionando classe ao elemento
+
+let meuParag = document.getElementById('meuParag')
+
+meuParag.classList.add('vermelho')
