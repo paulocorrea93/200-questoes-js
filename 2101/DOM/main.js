ccfc40025cd20906554cf86c90ac40ea29f39940
Paulo <paulocorreapc93@gmail.com>
2024-01-21 10:54:56 -0400

    adicionando tag html pelo DOM

--- 2101/DOM/main.js
+++ 2101/DOM/main.js
@@ -2,6 +2,12 @@
 
 // alteração de texto
 
-let novoTitulo = document.getElementById('main-header')
+let novoTitulo = document.getElementById("main-header");
 
-novoTitulo.textContent = "Novo título"
+novoTitulo.textContent = "Novo título";
+
+// adicionado tag pelo DOM
+
+let novaDiv = document.getElementById("main");
+
+novaDiv.innerHTML = "<p>Novo parágrafo.</p>";
