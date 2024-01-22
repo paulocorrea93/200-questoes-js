9db45c5b73f22434061f1d98edeb73880c7d8d4f
Paulo <paulocorreapc93@gmail.com>
2024-01-22 10:12:21 -0400

    exeercício 02 - alteração de conteúdo do elemento

--- 2201/DOM - exercícios/main.js
+++ 2201/DOM - exercícios/main.js
@@ -1,12 +1,17 @@
 // exercício 01
 
+let mainHeader = document.getElementById('main-header')
 let divMain = document.getElementById("main");
 let myButton = document.getElementById("myButton");
-let myParag = document.getElementById("myParag");
+let myparaMouseParag = document.getElementById("paraMouse");
 let myForm = document.getElementById("myForm");
 let myInput = document.getElementById("myInput");
 
 // exercício 02
+
+mainHeader.textContent = "Títilo 2"
+paraMouse.textContent = "Mickey Mouse"
+divMain.innerHTML = '<p>Parágrafo 2</p>'
 
 // exercício 03
 
