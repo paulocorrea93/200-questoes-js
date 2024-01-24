6cdf9a740ef2cea7712ebc5434d59ae287827ab4
Paulo <paulocorreapc93@gmail.com>
2024-01-24 10:51:24 -0400

    exercício 04 - validação de checkbox

--- 2401/exercícios/main.js
+++ 2401/exercícios/main.js
@@ -1,14 +1,18 @@
-let nomeForm = document.getElementById("nomeForm");
-let emailForm = document.getElementById("emailForm");
-let passForm = document.getElementById("passForm");
+// let nomeForm = document.getElementById("nomeForm");
+// let emailForm = document.getElementById("emailForm");
+// let passForm = document.getElementById("passForm");
+// let checkForm = document.getElementById('checkForm')
+
+let jsForm = document.getElementById('jsForm')
 
 let emailText = document.getElementById("emailText");
 let nome = document.getElementById("nome");
 let passText = document.getElementById("passText");
+let checkBox = document.getElementById('checkBox')
 
 let regex = /^(?=.*[A-Za-z])(?=.*\d).{8,}$/;
 
-nomeForm.addEventListener("submit", (e) => {
+jsForm.addEventListener("submit", (e) => {
   e.preventDefault();
 
   if (nome.value === "") {
@@ -16,7 +20,7 @@ nomeForm.addEventListener("submit", (e) => {
   }
 });
 
-emailForm.addEventListener("submit", (e) => {
+jsForm.addEventListener("submit", (e) => {
   e.preventDefault();
   if (!emailText.value.includes("@") || !emailText.value.includes(".")) {
     alert("E-mail inválido.");
@@ -25,9 +29,15 @@ emailForm.addEventListener("submit", (e) => {
 
 // exercício 03
 
-passForm.addEventListener('submit', (e)=> {
+jsForm.addEventListener('submit', (e)=> {
   e.preventDefault()
   if(!regex.test(passText.value)){
     alert("A senha deve conter 8 caracteres, 1 letra e 1 número")
   }
+})
+
+jsForm.addEventListener('submit', (e) => {
+  e.preventDefault()
+  if(!checkBox.checked)
+  alert("Você concordou.")
 })
