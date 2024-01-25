ef2946de86a2cab4742857770cd4770f6e8d1183
Paulo <paulocorreapc93@gmail.com>
2024-01-25 11:38:18 -0400

    armazenando no local storage com JSON

--- 2501/jsonimprove/main.js
+++ 2501/jsonimprove/main.js
@@ -1,17 +1,31 @@
-  // convertendo JSON em objeto
+// convertendo JSON em objeto
 
-  let json = '{"nome":"Paulo", "idade": 30, "cidade": "Manaus"}'
-  let objJson = JSON.parse(json)
-  console.log(objJson)
+let json = '{"nome":"Paulo", "idade": 30, "cidade": "Manaus"}';
+let objJson = JSON.parse(json);
+console.log(objJson);
 
-  // convertendo objeto em JSON
+// convertendo objeto em JSON
 
-  let objJson2 = {
-    sobrenome: "Correa",
-    mesDeNiver: 5,
-    estado: "Amazonas"
-  }
+let objJson2 = {
+  sobrenome: "Correa",
+  mesDeNiver: 5,
+  estado: "Amazonas",
+};
 
-  let paraJson = JSON.stringify(objJson2)
+let paraJson = JSON.stringify(objJson2);
 
-  console.log(paraJson)
+console.log(paraJson);
+
+// localStorage
+
+let pessoa = {
+  nome: "Peter",
+  idade: 35,
+  cidade: "New York",
+};
+
+localStorage.setItem('pessoa', JSON.stringify(pessoa))
+
+let pessoaRecuperada = JSON.parse(localStorage.getItem('pessoa'))
+
+console.log(pessoaRecuperada)
