a51be87854e0cb8f1427c8205691589c2d257ce6
Paulo <paulocorreapc93@gmail.com>
2024-01-28 15:24:35 -0400

    recuperando valor com evento de clique

--- 2501/json-exercicio/main.js
+++ 2501/json-exercicio/main.js
@@ -1,34 +1,42 @@
 // criar objeto com varias propriedades
 
 let aluno = {
-    nome: "Paulo",
-    idade: 30,
-    matriculado: true,
-    nMatricula: 1234,
-    curso: "ciência da computação"
-}
+  nome: "Paulo",
+  idade: 30,
+  matriculado: true,
+  nMatricula: 1234,
+  curso: "ciência da computação",
+};
 
 // converção do objeto em JSON
 
-let alunoJSON = JSON.stringify(aluno)
+let alunoJSON = JSON.stringify(aluno);
 
-console.log(alunoJSON)
+console.log(alunoJSON);
 
 // conversão do JSON em Objeto
 
-jsonAluno = JSON.parse(alunoJSON)
+jsonAluno = JSON.parse(alunoJSON);
 
-console.log(jsonAluno)
+console.log(jsonAluno);
 
 // importação de elementos
 
-let meuBotao = document.getElementById('meuBotao')
+let meuBotao = document.getElementById("meuBotao");
+let btnRecup = document.getElementById("btn-recup");
 
 // salvando entrada no armazenamento local
 
-meuBotao.addEventListener('click', (e)=> {
-    e.preventDefault()
-    let meuTexto = document.getElementById('meuTexto').value
-    localStorage.setItem("meuTexto", meuTexto)
-    console.log(localStorage)
-})
+meuBotao.addEventListener("click", (e) => {
+  e.preventDefault();
+  let meuTexto = document.getElementById("meuTexto").value;
+  localStorage.setItem("meuTexto", meuTexto);
+  console.log(localStorage);
+});
+
+btnRecup.addEventListener("click", (e) => {
+  e.preventDefault();
+  let meuTexto = document.getElementById("meuTexto").value;
+  let valor = localStorage.getItem("meuTexto", meuTexto)
+  alert(valor)
+});
