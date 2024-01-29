aaa2f8d44e36c42f32dd9d46a887615d1be03713
Paulo <paulocorreapc93@gmail.com>
2024-01-29 11:09:00 -0400

    fetch API em requisição GET

--- 2901/exercicios asyncAwait/main.js
+++ 2901/exercicios asyncAwait/main.js
@@ -1,11 +1,18 @@
 // função que retorna depois de 2 segundos
 
-function esperaDoisSegundos(){
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve("Tempo esgotado")
-        }, 2000)
-    })
+function esperaDoisSegundos() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("Tempo esgotado");
+    }, 2000);
+  });
 }
 
-esperaDoisSegundos().then(console.log)
+esperaDoisSegundos().then(console.log);
+
+// fetch API em requisição GET
+
+fetch("https://jsonplaceholder.typicode.com/users")
+  .then((response) => response.json)
+  .then((data) => console.log(data))
+  .catch((erro) => console.log(`Erro: ${erro}`));
