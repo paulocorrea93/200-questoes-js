a1b0868b04d0da2443e4d388a58adcf12d6aaeda
Paulo <paulocorreapc93@gmail.com>
2024-01-29 11:15:59 -0400

    função assincrona de requisição GET

--- 2901/exercicios asyncAwait/main.js
+++ 2901/exercicios asyncAwait/main.js
@@ -16,3 +16,13 @@ fetch("https://jsonplaceholder.typicode.com/users")
   .then((response) => response.json)
   .then((data) => console.log(data))
   .catch((erro) => console.log(`Erro: ${erro}`));
+
+// função assincrona de requisição GET
+
+async function getPrimeiroTitulo() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const data = await response.json();
+  return data[0].title;
+}
+
+getPrimeiroTitulo().then(console.log)
