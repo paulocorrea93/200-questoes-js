1d647367d2f51ccd9c1ff76164efaf471def83a1
Paulo <paulocorreapc93@gmail.com>
2024-01-29 11:18:09 -0400

    fetch API para requisição de POST

--- 2901/exercicios asyncAwait/main.js
+++ 2901/exercicios asyncAwait/main.js
@@ -25,4 +25,21 @@ async function getPrimeiroTitulo() {
   return data[0].title;
 }
 
-getPrimeiroTitulo().then(console.log)
+getPrimeiroTitulo().then(console.log);
+
+// fetch API de requisição POST
+
+fetch("https://jsonplaceholder.typicode.com/posts", {
+  method: "POST",
+  body: JSON.stringify({
+    title: "Meu Post",
+    body: "Este é o corpo do meu post",
+    userId: 1,
+  }),
+  headers: {
+    "Content-type": "application/json;charset=UTF-8",
+  },
+})
+  .then((response) => response.json())
+  .then((data) => console.log(data))
+  .catch((erro) => `Erro: ${erro}`);
